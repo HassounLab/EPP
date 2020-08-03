c595d69d18c30ecf26b2ac16046cfdd023979480
gvisani <43044242+gvisani@users.noreply.github.com>
2020-08-03 15:27:21 -0400

    minor updates train_tree_and_flat

--- src/train_tree_and_flat.py
+++ src/train_tree_and_flat.py
@@ -1,13 +1,17 @@
 import argparse
+import os
 
 from utils import pickle_load, pickle_dump
 from TreeAndFlatModels import TreeAndFlatModels
+
+MIN_POSITIVE_EXAMPLE = 10
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--inhibitors', default="False")
     parser.add_argument('--similarity', default="True")
     parser.add_argument('--ratio', default="all")
+    parser.add_argument('--output_folder', default='./')
     args = parser.parse_args()
 
     if args.similarity.lower() == "true":
@@ -27,13 +31,14 @@ if __name__ == "__main__":
         exit(1)
 
     r = args.ratio
+    output_folder = args.output_folder
 
     train_filepath = "../data/tree_data/tree_and_flat_data%s_train_r%s.pkl" % (inh, r)
     test_filepath = "../data/tree_data/tree_and_flat_data%s_test.pkl" % (inh)
 
     print("Creating Tree...")
-    model = TreeAndFlatModels(train_filepath = train_filepath, test_filepath = test_filepath, similarity= True, min_positive_examples = 10)
+    model = TreeAndFlatModels(train_filepath = train_filepath, test_filepath = test_filepath, similarity= True, min_positive_examples = MIN_POSITIVE_EXAMPLE)
     model.create_ec_numbers_tree()
     model.create_tree()
     print("Dumping Tree...")
-    pickle_dump(model, "../models/tree_classifier%s%s_r%s.pkl" % (sim, inh, r))
+    pickle_dump(model, os.path.join(output_folder, "../models/tree_classifier%s%s_r%s.pkl" % (sim, inh, r)))
